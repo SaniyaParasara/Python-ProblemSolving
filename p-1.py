9c6b9cd61cb8fc50ad3c82b09a83a17667845273
SaniyaParasara <saniyaparasara@gmail.com>
2026-07-27 10:41:57 +0530

    Delete p-1.py

--- p-1.py
+++
@@ -1,8 +0,0 @@
-# Program to demonstrate basic input and output operations
-
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-print("Name:", name)
-print("Age:", age)
-print("Hello", name, "! You are", age, "years old.")
