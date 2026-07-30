db377d9ca5095d75be46a06baa9a8b9dd4e4ff22
saniyasherasiya55 <sherashiyasaniya00@gmail.com>
2026-07-30 12:15:49 +0530

    added p-7.py

---
+++ p-7.py
@@ -0,0 +1,5 @@
+#7. Write a program to create a dictionary and demonstrate dictionary methods and iteration. 
+d = {'name': 'Saniya', 'age': 22, 'city': 'Rajkot'}
+print("Dictionary:", d)
+d.update({"age": 23})
+d["course"]= "Python"
