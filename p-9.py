2b66fda863de811945d20e21ea31aebe6ff4d654
saniyasherasiya55 <sherashiyasaniya00@gmail.com>
2026-07-30 12:59:50 +0530

    added p-9.py

---
+++ p-9.py
@@ -0,0 +1,7 @@
+#9. Write a program to define and use user-defined functions with different types of arguments. 
+def add(a, b):
+    return a + b
+print("Addition of 5 and 3 is:", add(5, 3)) 
+
+
+
