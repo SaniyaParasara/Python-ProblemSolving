95231803c9f3f6dcbe75cd3d4570f32ff8d283bb
Your Name <your-email@example.com>
2026-07-30 12:00:49 +0530

    added p-6.py

---
+++ ai-1.py
@@ -0,0 +1,100 @@
+from collections import degue
+#function to generate possible next states
+def get_next_states(state):
+    a,b = state
+
+    #capacities of jugs
+    jugA = 4
+    jugB = 3
+
+    states = []
+
+    #fill jug A completely
+    states.append((jugA,b))
+
+    #fill jug B completely
+    states.append((a,jugB))
+
+    #Empty Jug A
+    states.append((0,b))
+
+    #Empty Jug B
+    states.append((a,0))
+
+    #pour water from A to B
+    pour = min(a,jugB-b)
+    states.append(a-pour,b+pour)
+
+    
+    #pour water from A to B
+    pour = min(a,jugB-b)
+    states.append(a+pour,b-pour)
+
+    return states
+
+#BFS implementation
+def bfs():
+    start = (0,0)
+    goal = 2
+
+    #queue for BFS
+    queue = degue()
+
+    #store state and path
+    queue.append((start,[]))
+
+    #to avoid visiting same states
+    visited = set()
+
+    while queue():
+        current, path = queue.popft()
+        a,b = current
+
+        #if goal found
+        if a == goal:
+            return path +[current]
+
+        #Mark state visited
+        if current in visited:
+            continue
+        visited.add(current)
+        
+        #generate child states 
+        for next_state in get_next_states(current):
+            if next_state not in visited
+                queue.append((next_state,path +[current]))
+            return none
+
+        #run BFS
+        solution = bfs()
+        print('steps to reach goal :')
+
+        for step in solution:
+            print(step)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
---
+++ p-6.py
@@ -0,0 +1,8 @@
+# 6. Write a program to illustrate the use of tuples and sets with basic operations. 
+t = (1,2,3,4,5)
+print("Tuple:", t)
+print("Length of tuple:", len(t))
+print("First element of tuple:", t[0])
+print("Last element of tuple:", t[-1])
+print("count of 2 in tuple:", t.count(2))
+print("Index of 3 in tuple:", t.index(3))
