0a6b5a0584a3e736876d8f0feaa780bbcc7b8e8a
Saniya Parasara <saniyaparasara@gmail.com>
2026-08-06 18:09:38 +0530

    Clean up comments in number check program
    Removed comments explaining nested if conditions.

--- Unit_2/2.py
+++ Unit_2/2.py
@@ -4,14 +4,6 @@
 # Write a program to check whether a number is
 # Positive, Negative or Zero using nested conditions.
 
-# Definition:
-# Nested if means writing one if statement inside another if statement.
-# It is used when one condition depends on another condition.
-# In this program, we first check whether the number is
-# greater than or equal to zero.
-# If yes, we again check whether it is zero or positive.
-# Otherwise, the number is negative.
-
 # Taking input from the user
 num = int(input("Enter a number: "))
 
