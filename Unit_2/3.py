640e32256d11d01ddb9fc553ee54a2b94087e595
Saniya Parasara <saniyaparasara@gmail.com>
2026-08-06 18:10:00 +0530

    Clean up comments in multiplication table program
    Removed unnecessary comments explaining the for loop.

--- Unit_2/3.py
+++ Unit_2/3.py
@@ -4,12 +4,6 @@
 # Write a program to generate a multiplication table
 # using a for loop.
 
-# Definition:
-# A for loop is used to execute a block of code
-# repeatedly for a fixed number of times.
-# In this program, the for loop generates the
-# multiplication table of a given number.
-
 # Taking input from the user
 num = int(input("Enter a number: "))
 
