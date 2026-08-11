03d9440f5d5b3843fdaee98fb7c54e02e30564a3
Saniya Parasara <saniyaparasara@gmail.com>
2026-08-11 12:11:20 +0530

    Refactor generator function example in 10.py
    Removed comments and cleaned up code structure.

--- Unit_2/10.py
+++ Unit_2/10.py
@@ -1,21 +1,3 @@
-# Practical No. 10
+'''10. Write a program to generate a sequence of numbers
+using generator functions and the yield keyword.'''
 
-# Question:
-# Write a program to generate a sequence of numbers
-# using generator functions and the yield keyword.
-
-# Generator Function
-def numbers():
-
-    # Generate numbers from 1 to 5
-    for i in range(1, 6):
-        yield i
-
-# Create generator object
-gen = numbers()
-
-print("Generated Numbers:")
-
-# Print generated values
-for value in gen:
-    print(value)
